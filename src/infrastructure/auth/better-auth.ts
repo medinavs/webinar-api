cb7a4e65d9e6dd49edbec0fa6f561658ac2ec7d3
murilo <murilorockstar492@gmail.com>
2025-09-30 16:36:21 -0300

    feat: add advanced cookie configuration for better-auth session management

--- src/infrastructure/auth/better-auth.ts
+++ src/infrastructure/auth/better-auth.ts
@@ -5,7 +5,6 @@ import { prisma } from "../database/client";
 import { env } from "../config/env";
 import { FIVE_MINUTES_IN_SEC, ONE_WEEK_IN_SEC } from "@/shared/constants/time";
 
-
 export const auth = betterAuth({
     trustedOrigins: ['http://localhost:8080', 'http://localhost:5173', 'http://localhost:3000'],
     plugins: [admin()],
@@ -13,6 +12,24 @@ export const auth = betterAuth({
         provider: 'postgresql',
         usePlural: false,
     }),
+    advanced: {
+        cookiePrefix: "better-auth",
+        crossSubDomainCookies: {
+            enabled: true,
+        },
+        cookies: {
+            sessionToken: {
+                name: "better-auth.session_token",
+                options: {
+                    httpOnly: true,
+                    sameSite: "none",
+                    secure: true,
+                    path: "/",
+                    maxAge: 60 * 60 * 24 * 7, // 7 days
+                },
+            },
+        },
+    },
     emailAndPassword: {
         enabled: true,
         autoSignIn: true,
@@ -32,5 +49,5 @@ export const auth = betterAuth({
     logger: {
         level: "debug",
         disabled: env.IS_DEVELOP_MODE ? false : true,
-    },
+    }
 })
